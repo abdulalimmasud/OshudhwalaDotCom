066a4370530f0a9ecaba52e6d3289b4c6ead5e3d
Abdul Alim <abdulalimmasud@gmail.com>
2016-11-09 22:10:45 +0600

    Add Dapper for database access

---
+++ OshudhwalaDotCom/Core/DAL/MedicineGateway.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using System.Linq;
+using OshudhwalaDotCom.Models;
+
+namespace OshudhwalaDotCom.Core.DAL
+{
+    public class MedicineGateway
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["PharmacyDatabase"].ConnectionString;
+    }
+}
